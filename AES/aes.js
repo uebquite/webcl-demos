234af7e20666f20324c00bace2301860c4f74202
Afonso R. Costa Jr <arabelo@gmail.com>
2013-06-14 15:30:48 -0400

    Port AES to SRA's WebCL implementation
    Issue #17

--- AES/aes.js
+++ AES/aes.js
@@ -50,48 +50,38 @@ function InitCL()
         encData[i] = 0;
         decData[i] = 0;
     }
-    
+
     // Fill our key with initial values
     //
     for(var i = 0; i < KEYSIZE/4; i++)
         roundKey[i] = i;
 
-    if(typeof(WebCLComputeContext) === "undefined") {
-        console.error("WebCLComputeContext is yet to be undefined");
+    if(webcl === "undefined") {
+        console.error("WebCL is yet to be undefined");
         return null;
     }
 
-	cl = new WebCLComputeContext();
+    cl = webcl;
 
 	if(cl === null)
 	{
 		console.error("Failed to create WebCL context");
 		return;
 	}
-    console.log("WebCL: WebCLComputeContext created");
+    console.log("WebCL created");
 
-	platform_ids = cl.getPlatformIDs();
-	if (cl.getError() !== cl.SUCCESS)
-	{
-		console.error("Failed to get platform IDs");
-		return;
-	}
+	platform_ids = cl.getPlatforms();
 	if(platform_ids.length === 0)
 	{
 		console.error("No platforms available");
 		return;
 	}
-	platform_id = platform_ids[0]; 
+	platform_id = platform_ids[0];
 
 	// Connect to a compute device
 	//
 	var gpu = true;
-	device_ids = cl.getDeviceIDs(platform_id, gpu ? cl.DEVICE_TYPE_GPU : cl.DEVICE_TYPE_CPU);
-	if (cl.getError() !== cl.SUCCESS)
-	{
-		console.error("Failed to get device IDs");
-		return;
-	}
+	device_ids = platform_id.getDevices(gpu ? cl.DEVICE_TYPE_GPU : cl.DEVICE_TYPE_CPU);
 	if(device_ids.length === 0)
 	{
 		console.error("No devices available");
@@ -101,17 +91,20 @@ function InitCL()
 
 	// Create a compute context
 	//
-	context = cl.createContext(null, device_id, null, null);
-	if (cl.getError() !== cl.SUCCESS)
+    var contextProperties = {platform: platform_id, devices: device_id,
+                             deviceType: gpu ? cl.DEVICE_TYPE_GPU : cl.DEVICE_TYPE_CPU,
+                             shareGroup: 0, hint: null};
+	context = cl.createContext(contextProperties);
+	if (!context)
 	{
-		console.error("Failed to create a compute context");
+		console.error("Failed to create a WebCL context");
 		return;
 	}
 
 	// Create a command queue
 	//
-	queue = cl.createCommandQueue(context, device_id, null);
-	if (cl.getError() !== cl.SUCCESS)
+	queue = context.createCommandQueue(device_id);
+	if (!queue)
 	{
 		console.error("Failed to create a command queue");
 		return;
@@ -125,36 +118,35 @@ function InitCL()
         console.error("No kernel named: " + "AESEncryptDecrypt");
 		return;
 	}
-	program = cl.createProgramWithSource(context, kernelSource);
-	if (cl.getError() !== cl.SUCCESS)
+	program = context.createProgram(kernelSource);
+	if (!program)
 	{
-		console.error("Failed to create compute program");
+		console.error("Failed to create WebCL program");
 		return;
 	}
 
 	// Build the program executable
 	//
-	cl.buildProgram(program, null, null, null);
-	if (cl.getError() !== cl.SUCCESS)
-	{
-		console.error("Failed to build program executable");
-		var info = cl.getProgramBuildInfo(program, device_id, cl.PROGRAM_BUILD_LOG);
-		console.log(info);
-		return;
-	}
+    try {
+        program.build(device_id);
+    } catch(e) {
+        console.error("Failed to build WebCL program. Error: " +
+        program.getBuildInfo(device_id, cl.PROGRAM_BUILD_LOG));
+        return;
+    }
 
 	// Create compute kernels in the program we wish to run
 	//
-    kernelEncrypt = cl.createKernel(program, "AESEncrypt");
-	if (cl.getError() !== cl.SUCCESS)
+    kernelEncrypt = program.createKernel("AESEncrypt");
+	if (!kernelEncrypt)
 	{
         console.error("Failed to create AESEncrypt kernel");
 		return;
 	}
     console.log("WebCL: kernelEncrypt created");
-  
-    kernelDecrypt = cl.createKernel(program, "AESDecrypt");
-    if (cl.getError() !== cl.SUCCESS)
+
+    kernelDecrypt = program.createKernel("AESDecrypt");
+    if (!kernelDecrypt)
     {
         console.error("Failed to create AESDecrypt kernel");
         return;
@@ -163,190 +155,137 @@ function InitCL()
 
 	// Create the memory buffers for our calculation
 	//
-    freeBuf = cl.createBuffer(context, cl.MEM_READ_ONLY,  Int32Array.BYTES_PER_ELEMENT * NINTS, null);
-    encBuf  = cl.createBuffer(context, cl.MEM_READ_WRITE, Int32Array.BYTES_PER_ELEMENT * NINTS, null);
-    decBuf  = cl.createBuffer(context, cl.MEM_WRITE_ONLY, Int32Array.BYTES_PER_ELEMENT * NINTS, null);
+    freeBuf = context.createBuffer(cl.MEM_READ_ONLY,  Int32Array.BYTES_PER_ELEMENT * NINTS);
+    encBuf  = context.createBuffer(cl.MEM_READ_WRITE, Int32Array.BYTES_PER_ELEMENT * NINTS);
+    decBuf  = context.createBuffer(cl.MEM_WRITE_ONLY, Int32Array.BYTES_PER_ELEMENT * NINTS);
     if (freeBuf === null || encBuf === null || decBuf === null)
 	{
         console.error("Failed to allocate freeBuf or encBuf or decBuf");
 		return;
 	}
-    
-    roundKeyBuf = cl.createBuffer(context, cl.MEM_READ_ONLY, KEYSIZE, null);
+
+    roundKeyBuf = context.createBuffer(cl.MEM_READ_ONLY, KEYSIZE);
     if (roundKeyBuf === null)
     {
         console.error("Failed to allocate roundKeyBuf");
         return;
     }
-    
+
     // Get the maximum work group size for executing the kernel on the device
     //
-    var maxWorkGroupSize = cl.getKernelWorkGroupInfo(kernelEncrypt, device_id, cl.KERNEL_WORK_GROUP_SIZE);
-    if (cl.getError() !== cl.SUCCESS)
-    {
-        console.error("Failed to retrieve kernel work group info");
-        return;
-    }
+    var maxWorkGroupSize = kernelEncrypt.getWorkGroupInfo(device_id, cl.KERNEL_WORK_GROUP_SIZE);
     console.log("WebCL: maxWorkGroupSize: " + maxWorkGroupSize);
 
     globalThreads[0] = dataWidth;
     globalThreads[1] = dataHeight;
-    
+
     localThreads[0] = 64;
     localThreads[1] = 4;
-    
+
     if(localThreads[0] * localThreads[1] > maxWorkGroupSize)
     {
         console.error("Workgroup size too large");
         return;
     }
-    
+
     HideResults();
 }
 
 function EncryptCL(rounds)
 {
     tStart = new Date().valueOf();
-    
+
     // Write our data set into the input array in device memory
 	//
-    cl.enqueueWriteBuffer(queue, freeBuf, true, 0, Int32Array.BYTES_PER_ELEMENT * NINTS, freeData, null);
-	if (cl.getError() !== cl.SUCCESS)
-	{
-		console.error("Failed to write encbuf");
-		return;
-	}
-    
-    cl.enqueueWriteBuffer(queue, roundKeyBuf, true, 0, KEYSIZE, roundKey, null);
-    if (cl.getError() !== cl.SUCCESS)
-    {
-        console.error("Failed to write roundKeyBuf");
-        return;
-    }
+    queue.enqueueWriteBuffer(freeBuf, true, 0, Int32Array.BYTES_PER_ELEMENT * NINTS, freeData);
+
+    queue.enqueueWriteBuffer(roundKeyBuf, true, 0, KEYSIZE, roundKey);
     console.log("WebCL-enc: OK enqueueWriteBuffer");
 
 	// Set the arguments to our compute kernel
 	//
-    cl.setKernelArgGlobal(kernelEncrypt, 0, encBuf);
-    cl.setKernelArgGlobal(kernelEncrypt, 1, freeBuf);
-    cl.setKernelArgGlobal(kernelEncrypt, 2, roundKeyBuf);
+    kernelEncrypt.setArg(0, encBuf);
+    kernelEncrypt.setArg(1, freeBuf);
+    kernelEncrypt.setArg(2, roundKeyBuf);
 
-    cl.setKernelArgLocal(kernelEncrypt, 3, localThreads[0] * localThreads[1] * 4);  // from AMD code
-    cl.setKernelArgLocal(kernelEncrypt, 4, localThreads[0] * localThreads[1] * 4);  // from AMD code
-    
-    cl.setKernelArg(kernelEncrypt, 5, rounds, cl.KERNEL_ARG_UINT);
-      
-	if (cl.getError()!== cl.SUCCESS)
-	{
-		console.error("Failed to set kernel arguments");
-		return;
-	}
+    kernelEncrypt.setArg(3, localThreads[0] * localThreads[1] * 4);  // from AMD code
+    kernelEncrypt.setArg(4, localThreads[0] * localThreads[1] * 4);  // from AMD code
+
+    kernelEncrypt.setArg(5, rounds, WebCLKernelArgumentTypes.UINT);
 
 	// Execute the kernel over the entire range of our 2d input data set
 	//
-    cl.enqueueNDRangeKernel(queue, kernelEncrypt, 2, 0, globalThreads, localThreads, null);
-	if (cl.getError() !== cl.SUCCESS)
-	{
-		console.error("Failed to execute kernel");
-		return;
-	}
+    queue.enqueueNDRangeKernel(kernelEncrypt, 0, globalThreads, localThreads);
     console.log("WebCL-enc: OK enqueueNDRangeKernel");
 
 	// Wait for the command queue to get serviced before reading back results
 	//
-    cl.finish(queue, OnEncryptComplete, cl);
+    queue.finish();
+    OnEncryptComplete();
 }
 
-function  OnEncryptComplete(userData)
+function  OnEncryptComplete()
 {
 	// Read back the results from the device to verify the output
 	//
-    cl.enqueueReadBuffer(queue, encBuf, true, 0, Int32Array.BYTES_PER_ELEMENT * NINTS, encData, null);
+    queue.enqueueReadBuffer(encBuf, true, 0, Int32Array.BYTES_PER_ELEMENT * NINTS, encData);
+    queue.finish();
     tEnd = new Date().valueOf();
-	if (cl.getError() !== cl.SUCCESS)
-	{
-		console.error("Failed to read output array");
-		return;
-	}
     console.log("WebCL-enc: OK enqueueReadBuffer");
-    
+
     //for(var i=0; i<10; i++)
     //    console.log("free[" + i + "]=" + freeData[i] + " enc[" + i + "]=" + encData[i] + " dec[" + i + "]=" + decData[i]);
-        
+
     ShowResults();
 }
 
 function DecryptCL(rounds)
 {
     tStart = new Date().valueOf();
-    
+
     // Write our data set into the input array in device memory
     //
-    cl.enqueueWriteBuffer(queue, encBuf, true, 0, Int32Array.BYTES_PER_ELEMENT * NINTS, encData, null);
-    if (cl.getError() !== cl.SUCCESS)
-    {
-        console.error("Failed to write freebuf");
-        return;
-    }
-    
-    cl.enqueueWriteBuffer(queue, roundKeyBuf, true, 0, KEYSIZE, roundKey, null);
-    if (cl.getError() !== cl.SUCCESS)
-    {
-        console.error("Failed to write roundKeyBuf");
-        return;
-    }
+    queue.enqueueWriteBuffer(encBuf, true, 0, Int32Array.BYTES_PER_ELEMENT * NINTS, encData);
+
+    queue.enqueueWriteBuffer(roundKeyBuf, true, 0, KEYSIZE, roundKey);
     console.log("WebCL-dec: OK enqueueWriteBuffer");
 
     // Set the arguments to our compute kernel
     //
-    cl.setKernelArgGlobal(kernelDecrypt, 0, decBuf);
-    cl.setKernelArgGlobal(kernelDecrypt, 1, encBuf);
-    cl.setKernelArgGlobal(kernelDecrypt, 2, roundKeyBuf);
+    kernelDecrypt.setArg(0, decBuf);
+    kernelDecrypt.setArg(1, encBuf);
+    kernelDecrypt.setArg(2, roundKeyBuf);
 
-    cl.setKernelArgLocal(kernelDecrypt, 3, localThreads[0] * localThreads[1] * 4);  // from AMD code
-    cl.setKernelArgLocal(kernelDecrypt, 4, localThreads[0] * localThreads[1] * 4);  // from AMD code
-    
-    cl.setKernelArg(kernelDecrypt, 5, rounds, cl.KERNEL_ARG_UINT);
-      
-    if (cl.getError()!== cl.SUCCESS)
-    {
-        console.error("Failed to set kernel arguments");
-        return;
-    }
+    kernelDecrypt.setArg(3, localThreads[0] * localThreads[1] * 4);  // from AMD code
+    kernelDecrypt.setArg(4, localThreads[0] * localThreads[1] * 4);  // from AMD code
+
+    kernelDecrypt.setArg(5, rounds, WebCLKernelArgumentTypes.UINT);
 
     // Execute the kernel over the entire range of our 2d input data set
     //
-    cl.enqueueNDRangeKernel(queue, kernelDecrypt, 2, 0, globalThreads, localThreads, null);
-    if (cl.getError() !== cl.SUCCESS)
-    {
-        console.error("Failed to execute kernel");
-        return;
-    }
+    queue.enqueueNDRangeKernel(kernelDecrypt, 0, globalThreads, localThreads);
     console.log("WebCL-dec: OK enqueueNDRangeKernel");
 
     // Wait for the command queue to get serviced before reading back results
     //
-    cl.finish(queue, OnDecryptComplete, cl);
+    queue.finish();
+    OnDecryptComplete();
 }
 
-function  OnDecryptComplete(userData)
+function  OnDecryptComplete()
 {
     // Read back the results from the device to verify the output
     //
-    cl.enqueueReadBuffer(queue, decBuf, true, 0, Int32Array.BYTES_PER_ELEMENT * NINTS, decData, null);
+    queue.enqueueReadBuffer(decBuf, true, 0, Int32Array.BYTES_PER_ELEMENT * NINTS, decData);
+    queue.finish();
     tEnd = new Date().valueOf();
-    if (cl.getError() !== cl.SUCCESS)
-    {
-        console.error("Failed to read output array");
-        return;
-    }
     console.log("WebCL-dec: OK enqueueReadBuffer");
-    
+
     //for(var i=0; i<10; i++)
     //    console.log("free[" + i + "]=" + freeData[i] + " dec[" + i + "]=" + decData[i]);
-        
+
     ShowResults();
-    
+
     // validate
     var badCnt = 0;
     for(var i=0; i<NINTS; i++) {
@@ -354,22 +293,22 @@ function  OnDecryptComplete(userData)
             badCnt++;
         }
     }
-    
+
     if(badCnt !== 0) console.error("AES fails");
-    
+
     console.log("total: " + NINTS + ", bad: " + badCnt);
 }
 
 function ShowResults()
 {
     console.log("delta: " + (tEnd - tStart) + " msec");
-    
+
     var delta = Math.max(1, tEnd - tStart);                             // msec
     var bps = 1000 * NINTS * Int32Array.BYTES_PER_ELEMENT / delta;      // bytes / sec
     var mbps = Math.floor(bps / (1024 * 1024));
-    
+
     document.getElementById("bps").firstChild.nodeValue = mbps + " MB/sec";
-    
+
     document.getElementById("res").style.visibility = "visible";
     document.getElementById("bps").style.visibility = "visible";
 }
@@ -379,5 +318,4 @@ function HideResults()
     document.getElementById("res").style.visibility = "hidden";
     document.getElementById("bps").style.visibility = "hidden";
 }
-
 
