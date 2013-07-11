dae52000d4994965e602eeea2c7ed7e8d2184e81
Diego Pinheiro <diego.qp@samsung.com>
2013-07-11 18:52:25 -0400

    Improve AES Demo
    Some changes were made to make this demo more readable.

--- AES/aes.js
+++ AES/aes.js
@@ -1,15 +1,14 @@
 var DATA_SIZE = 1024;
 
 var err;                                    // error code returned from API calls
-var dataWidth   = 4;
-var dataHeight  = 4;
-var NINTS       = dataWidth * dataHeight;
+var NINTS       = Math.pow(2,25);
 
 var text        = "1234567890123456";
-var textUint    = new Uint8Array(NINTS);
-var encData     = new Uint8Array(NINTS);
-var decData     = new Uint8Array(NINTS);
+var textUint;
+var encData;
+var decData;
 
+var rounds      = 10;
 var KEYSIZE     = 176;      // (rounds +1) * 128/8 = 11*16
 var roundKey    = new Uint8Array(KEYSIZE); // 176 bytes -> 44 words
 
@@ -35,14 +34,133 @@ var localThreads  = new Int32Array(DIM);      // workgroup thread count
 
 var tStart;
 var tEnd;
+var realText;
 
-function encryptCL(rounds) {
+function readTest(num) {
+
+    if (num == 0) {
+        document.getElementById("inputText").value = "";
+        document.getElementById("inputKey").value = "";
+    }
+    else {
+        document.getElementById("inputText").value = tests["test" + num];
+        document.getElementById("inputKey").value = tests["key" + num];
+    }
+}
+
+function hexToString(hex) {
+    var text;
+    var i;
+
+    text = [];
+    hex = hex.split(" ");
+    for (i = 0; i < hex.length; i++) {
+        text[i] = parseInt(hex[i], 16);
+    }
+
+    return text;
+}
+
+function getInputText() {
+    var text;
+    var key;
+    var textObject;
+    var keyObject;
+    var isPlainText;
+    var len; // textUint length: it's a multiple of 16 bytes
+             // padding zeros are add if necessary
+
+    /* Get Text */
+
+    textObject = document.getElementById("inputText");
+    if (textObject === null) {
+        return null;
+    }
+
+    text = textObject.value;
+    if (!text) {
+        return null;
+    }
+
+    isPlainText = document.getElementById("plainText").checked;
+    if (isPlainText) {
+        // get all ASCII code from the text's character
+        len = 16 * Math.ceil(text.length / 16);
+        textUint = new Uint8Array(len);
+        for (i = 0; i < text.length; i++) {
+            textUint[i] = text.charCodeAt(i);
+        }
+    }
+    else {
+        text = hexToString(text);
+        len = 16 * Math.ceil(text.length / 16);
+        textUint = new Uint8Array(len);
+        for (i = 0; i < text.length; i++) {
+            textUint[i] = text[i];
+        }
+    }
+
+    /* Get Secret Key */
+
+    keyObject = document.getElementById("inputKey");
+    if (keyObject === null) {
+        return null;
+    }
+
+    key = keyObject.value;
+    if (!key) {
+        return null;
+    }
+
+    if (isPlainText) {
+        if (key.length != 16) {
+            return null;
+        }
+        for (i = 0; i < 16; i++) {
+            roundKey[i] = key.charCodeAt(i);
+        }
+    }
+    else {
+        key = hexToString(key);
+        len = 16 * Math.ceil(key.length / 16);
+        for (i = 0; i < 16; i++) {
+            roundKey[i] = key[i];
+        }
+    }
+
+    for (i = 16; i < KEYSIZE; i++) {
+        roundKey[i] = i;
+    }
+
+    /*
+    for (i = 0; i < KEYSIZE; i++) {
+        roundKey[i] = i;
+    }
+    */
+
+    return textUint;
+}
+
+function encryptCL() {
+    var numBytes;
+
+    // uncomment this to read from input
+    //textUint = getInputText();
+    if (!textUint) {
+        alert("Invalid Text or Key Size");
+        return;
+    }
 
     try {
         tStart = new Date().valueOf();
+        numBytes = textUint.length;
+
+        // Initialize buffers
+        freeBuf = context.createBuffer(cl.MEM_READ_ONLY,  numBytes);
+        encBuf  = context.createBuffer(cl.MEM_READ_WRITE, numBytes);
 
         // Write our plain text into the input array in device memory
-        queue.enqueueWriteBuffer(freeBuf, true, 0, NINTS, textUint);
+        queue.enqueueWriteBuffer(freeBuf, true, 0, numBytes, textUint);
         queue.enqueueWriteBuffer(roundKeyBuf, true, 0, KEYSIZE, roundKey);
 
         // Set the arguments to our compute kernel
@@ -52,6 +170,7 @@ function encryptCL(rounds) {
         kernelEncrypt.setArg(3, rounds, WebCLKernelArgumentTypes.UINT);
 
         // Execute the kernel
+        globalThreads[0] = Math.ceil(numBytes / 16);
         queue.enqueueNDRangeKernel(kernelEncrypt, null, globalThreads, localThreads);
 
         // Wait for the command queue to get serviced before reading back results
@@ -63,21 +182,64 @@ function encryptCL(rounds) {
 }
 
 function onEncryptComplete() {
+    var i;
+    var encData;
+    var strResult;
+    var bytesToRead;
+    var isPlainText;
+
+    // variable to hold the results from the device
+    bytesToRead = textUint.length;
+    encData = new Uint8Array(bytesToRead);
 
     // Read back the encrypt text from the device
-    queue.enqueueReadBuffer(encBuf, true, 0, NINTS, encData);
+    queue.enqueueReadBuffer(encBuf, true, 0, bytesToRead, encData);
     tEnd = new Date().valueOf();
 
-    showResults(tStart, tEnd);
+    // handle result
+    strResult = "";
+    isPlainText = document.getElementById("plainText").checked;
+    if (isPlainText) {
+        for (i = 0; i < encData.length; i++) {
+            strResult += String.fromCharCode(encData[i]);
+        }
+    }
+    else {
+        for (i = 0; i < encData.length; i++) {
+            strResult += getBytes(encData[i]);
+            if (i+1 < encData.length) {
+                strResult += " ";
+            }
+        }
+    }
+
+    // uncomment this to write in output
+    //showResults(tStart, tEnd, "Encrypt Text", strResult);
+    showResults(tStart, tEnd, "Encrypt Text", "");
+    realText = textUint;
+    textUint = encData;
 }
 
-function decryptCL(rounds) {
+function decryptCL() {
+    var numBytes;
+
+    // uncomment this to read from input
+    //textUint = getInputText();
+    if (!textUint) {
+        alert("Invalid Text");
+        return;
+    }
 
     try {
         tStart = new Date().valueOf();
+        numBytes = textUint.length;
+
+        // Initialize buffers
+        encBuf  = context.createBuffer(cl.MEM_READ_ONLY, numBytes);
+        decBuf = context.createBuffer(cl.MEM_READ_WRITE,  numBytes);
 
         // Write our encrypt text into the input array in device memory
-        queue.enqueueWriteBuffer(encBuf, true, 0, NINTS, encData);
+        queue.enqueueWriteBuffer(encBuf, true, 0, numBytes, textUint);
         queue.enqueueWriteBuffer(roundKeyBuf, true, 0, KEYSIZE, roundKey);
 
         // Set the arguments to our compute kernel
@@ -87,6 +249,7 @@ function decryptCL(rounds) {
         kernelDecrypt.setArg(3, rounds, WebCLKernelArgumentTypes.UINT);
 
         // Execute the kernel over the entire range of our 2d input data set
+        globalThreads[0] = Math.ceil(numBytes / 16);
         queue.enqueueNDRangeKernel(kernelDecrypt, 0, globalThreads, localThreads);
 
         // Wait for the command queue to get serviced before reading back results
@@ -100,25 +263,55 @@ function decryptCL(rounds) {
 function onDecryptComplete() {
     var i;
     var badCnt;
+    var decData;
+    var strResult;
+    var bytesToRead;
+    var isPlainText;
+
+    // variable to hold the results from the device
+    bytesToRead = textUint.length;
+    decData = new Uint8Array(bytesToRead);
 
     // Read back the decrypt text from the device to verify the output
-    queue.enqueueReadBuffer(decBuf, true, 0, NINTS, decData);
+    queue.enqueueReadBuffer(decBuf, true, 0, bytesToRead, decData);
     tEnd = new Date().valueOf();
-
-    showResults(tStart, tEnd);
 
     // validate
     badCnt = 0;
     for (i = 0; i < NINTS; i++) {
-        if (textUint[i] !== decData[i]) {
+        if (realText[i] !== decData[i]) {
             badCnt++;
         }
     }
 
     if(badCnt !== 0) {
         console.error("AES fails");
+        console.error("Read: " + NINTS + ", Bad: " + badCnt);
     }
-    console.log("total: " + NINTS + ", bad: " + badCnt);
+    else {
+        console.log("AES Passed!");
+    }
+
+    // handle result
+    strResult = "";
+    isPlainText = document.getElementById("plainText").checked;
+    if (isPlainText) {
+        for (i = 0; i < decData.length; i++) {
+            strResult += String.fromCharCode(decData[i]);
+        }
+    }
+    else {
+        for (i = 0; i < decData.length; i++) {
+            strResult += getBytes(decData[i]);
+            if (i+1 < decData.length) {
+                strResult += " ";
+            }
+        }
+    }
+
+    // uncomment this to write in output
+    //showResults(tStart, tEnd, "Decrypt Text", strResult);
+    showResults(tStart, tEnd, "Decrypt Text", "");
 }
 
 // get kernel source
@@ -179,9 +372,6 @@ function initCL() {
         kernelEncrypt = program.createKernel("AESEncrypt");
         kernelDecrypt = program.createKernel("AESDecrypt");
 
-        freeBuf = context.createBuffer(cl.MEM_READ_ONLY,  NINTS);
-        encBuf  = context.createBuffer(cl.MEM_READ_WRITE, NINTS);
-        decBuf  = context.createBuffer(cl.MEM_WRITE_ONLY, NINTS);
         roundKeyBuf = context.createBuffer(cl.MEM_READ_ONLY, KEYSIZE);
 
         globalThreads[0] = Math.ceil(text.length / 16);
@@ -190,41 +380,41 @@ function initCL() {
         console.error("Error", e.message);
     }
 
+    // TEMPORARY PART
+    // Get the maximum work group size for executing the kernel on the device
+    var maxWorkGroupSize = kernelEncrypt.getWorkGroupInfo(device_id, cl.KERNEL_WORK_GROUP_SIZE);
+    console.error("WebCL: maxWorkGroupSize: " + maxWorkGroupSize);
+
     hideResults();
-    encryptCL(10);
-    decryptCL(10);
 }
 
 // return the hexadecimal representation in just one byte
 function getBytes(num) {
-    var bytes = [];
+    var bytes;
     var i;
 
     for (i = 0; i < 1; i++) {
-        bytes[i] = (num & 255).toString(16);
+        bytes = (num & 255).toString(16);
         num >>= 8;
     }
 
-    return bytes[0];
+    return bytes;
 }
 
 function initVars() {
     var tmp;
+    var len;
     var i;
 
-    // get all ASCII code from the text's character
-    for (i = 0; i < text.length; i++) {
-        textUint[i] = text.charCodeAt(i);
+    len = 16 * Math.ceil(NINTS / 16);
+    textUint = new Uint8Array(len);
+    for (i = 0; i < len; i++) {
+        textUint[i] = i;
     }
 
     // Fill round keys
     for(i = 0; i < KEYSIZE; i++) {
         roundKey[i] = i;
-    }
-
-    for(var i = 0; i < NINTS; i++) {
-        encData[i] = 0;
-        decData[i] = 0;
     }
 }
 
